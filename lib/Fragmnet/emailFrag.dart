3c8c3dd5e612de5d2663edcbcdaa0f3d605030e2
Iliyas Robin <iliyasahmedr@gmail.com>
2024-11-29 20:57:05 +0600

    updating

---
+++ lib/Fragmnet/emailFrag.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/cupertino.dart';
+
+class emailFrag extends StatelessWidget{
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Center(
+        child: Text("emailFrag"),
+      ),
+    );
+  }
+
+}
